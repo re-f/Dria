4c7e595f40ec34cf227d850f98175c2db0cc900d
zhouwenya <zhouwenya@actionsky.com>
2017-07-26 11:05:24 +0800

    add func: RandId

---
+++ string.go
@@ -0,0 +1,20 @@
+package dry
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+
+func RandId(prefix string, n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return prefix + string(b)
+}
